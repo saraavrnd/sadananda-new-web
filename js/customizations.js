19f395d86299f4d018890a5a2714efeef2816c78
Saraav <saraav.rnd@gmail.com>
2023-05-16 17:02:43 +0530

    updated with loading status and responsive

--- js/customizations.js
+++ js/customizations.js
@@ -1,5 +1,10 @@
 var app_url_prefix = `/sadananda-new-web`
 // var app_url_prefix = ''
+var load_results = {
+    'banner' : 0,
+    'initiative' : 0,
+    'about_us' : 0
+}
 loadBanners = function(page, cb) {
     console.log('Loading banner data for page:' + page)
     
@@ -41,7 +46,8 @@ loadBanners = function(page, cb) {
             }
         });
     } catch(ex) {
-        
+        console.log('Exception occurred while loading the banners data', ex)
+        cb(ex, undefined)
     }
 }
 
@@ -103,7 +109,8 @@ loadInitiatives = function(cb) {
             }
         });
     } catch(ex) {
-        
+        console.log('Exception occurred while loading the initiatives data', ex)
+        cb(ex, undefined)
     }
 }
 
@@ -150,3 +157,115 @@ renderInitiatives = function(id, menuid, initiatives) {
     })
     $(menuid).html(initiativeMenuHTML.join(''));
 }	
+
+loadAboutUs = function(cb) {
+    const url = `${app_url_prefix}/data/about_us.dat`;
+    console.log('Loading about us from:', url)
+
+    try {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            success: (data)  => {
+                // console.log("Banner data:", data);
+                const lines = data.split('\n').filter((line) => {
+                    return $.trim(line).length > 0 && (! $.trim(line).startsWith('#'))
+                })
+                // console.log('Lines', lines)
+                var about_us = {}
+                lines.forEach((line) => {
+                    const index = line.indexOf('=')
+                    if(index != -1) {
+                        
+                        const key = line.substring(0, index);
+                        const value = line.substring(index+1)
+                        about_us[key] = value
+                    }
+                })
+                console.log('About', about_us)
+                cb(undefined, about_us)
+            },
+            error: (request, status, error) => {
+                console.error('Failed to get the about ius data')
+                console.error('Request', request)
+                console.error('Status', status)
+                console.error('Error', error)
+                cb(error, undefined)
+            }
+        });
+    } catch(ex) {
+        console.log('Exception occurred while loading the about us data', ex)
+        cb(ex, undefined)
+    }
+}
+
+renderAboutUsAndSocialContacts = function(about_us_data) {
+
+    renderAboutUS('#anandam_mini_desc', 'ABOUT_ANANDAM_MIN_DESC', about_us_data)
+    renderAboutUS('#sadananda_mini_desc', 'ABOUT_SADANANDA_MIN_DESC', about_us_data)
+
+    renderAboutUS('#anandam_detailed_desc', 'ABOUT_ANANDAM_DETAILED_DESC', about_us_data)
+    renderAboutUS('#sadananda_detailed_desc', 'ABOUT_SADANANDA_DETAILED_DESC', about_us_data)
+    renderSocialContacts('#social_contacts_phone', 'CONTACT_PHONE', about_us_data)
+    renderSocialContacts('#social_contacts_fb', 'CONTACT_FACEBOOK_ID', about_us_data)
+    renderSocialContacts('#social_contacts_twitter', 'CONTACT_TWITTER_ID', about_us_data)
+    renderSocialContacts('#social_contacts_email', 'CONTACT_EMAIL_ID', about_us_data)
+
+    renderContactUS('#contact_address', 'CONTACT_ADDRESS', about_us_data)
+    renderContactUS('#contact_phone', 'CONTACT_PHONE', about_us_data)
+    renderContactUS('#contact_email', 'CONTACT_EMAIL_ID', about_us_data)
+
+    renderLegalClaim('#legalDisclaimer .modal-body', 'LEGAL_DISCLAIMER', about_us_data)
+}
+
+showLegalDisclaimer = function( ) {
+    // $('#legalDisclaimer').modal({
+    //     show: true,
+    //     focus: true
+    // })
+    $('#legalDisclaimer').modal('show')
+    console.log('Show legal')
+}
+
+renderLegalClaim = function(id, key, about_us_data) {
+    $(id).html(about_us_data[key])
+}
+
+renderContactUS = function(id, key, about_us_data) {
+    $(id).html(about_us_data[key])
+}
+
+renderAboutUS = function(id, key, about_us_data) {
+    $(id).html(about_us_data[key])
+}
+
+renderSocialContacts = function(id, key, about_us_data) {
+    if(about_us_data[key] && about_us_data[key].length > 0) {
+        $(id).attr("href", about_us_data[key])
+    } else {
+        $(id).hide();
+    }
+}
+
+showLoading = function(status) {
+    if(status) {
+        $('#loading-status').show()
+        $('.wrapper, .footer ').hide()
+    } else {
+        $('#loading-status').hide()
+        $('.wrapper, .footer').show()
+    }
+}
+loadstatus = function(step, result) {
+
+    showLoading(true);
+    load_results[step] = result;
+
+    if(load_results['banner'] == 1 
+        && load_results['initiative'] == 1 
+        && load_results['about_us'] == 1 
+    ) {
+        showLoading(false);
+    }
+
+}
